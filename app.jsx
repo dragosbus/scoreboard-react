a4728b7721ceb12b60051f9d30ee0523146d1203
dragosbus <dragosbusuioc94@gmail.com>
2018-04-23 22:34:03 +0200

    added propType for title of the header and a default value of ScoreBoard

--- app.jsx
+++ app.jsx
@@ -1,17 +1,17 @@
 const App = () => {
     return (
         <div className="scoreboard">
-            <Header />
-            <Player name="Dragos" score="40"/>
-            <Player name="Mihail" score="30"/>
+            <Header title={3}/>
+            <Player name="Dragos" score={40}/>
+            <Player name="Mihail" score={30}/>
         </div>
     );
 };
 
-const Header = () => {
+const Header = props => {
     return (
         <header className="header">
-            <h1>ScoreBoard</h1>
+            <h1>{props.title}</h1>
         </header>
     );  
 };
@@ -19,9 +19,7 @@ const Header = () => {
 const Player = props => {
     return (
         <div className="player">
-            <div className="player-name">
-                {props.name}
-                </div>
+            <div className="player-name">{props.name}</div>
             <div className="player-score">
                 <div className="counter">
                     <button className="counter-action decrement">-</button>
@@ -31,6 +29,14 @@ const Player = props => {
             </div>
         </div>
     );  
+};
+
+Header.propTypes = {
+    title: React.PropTypes.string
+};
+
+App.defaulProps = {
+    title:"Scoreboard"
 };
 
 ReactDOM.render(
