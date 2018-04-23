0c547640dc4e9d16159c82ea98d2345b4eac1f50
dragosbus <dragosbusuioc94@gmail.com>
2018-04-23 22:19:02 +0200

    create a component Header and a component Player

--- app.jsx
+++ app.jsx
@@ -1,7 +1,39 @@
 const App = () => {
     return (
-        <div className="app">
-        
+        <div className="scoreboard">
+            <Header />
+            <Player name="Dragos" score="40"/>
+            <Player name="Mihail" score="30"/>
         </div>
     );
 };
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>ScoreBoard</h1>
+        </header>
+    );  
+};
+
+const Player = props => {
+    return (
+        <div className="player">
+            <div className="player-name">
+                {props.name}
+                </div>
+            <div className="player-score">
+                <div className="counter">
+                    <button className="counter-action decrement">-</button>
+                    <div className="counter-score">{props.score}</div>
+                    <button className="counter-action increment">+</button>
+                </div>
+            </div>
+        </div>
+    );  
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('container')
+);
